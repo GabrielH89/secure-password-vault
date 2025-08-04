f90594be8ae3092f87fbb080cb4fa66624f26b16
Gabriel Henrique <gabrielhenriquegh3878@gmail.com>
2025-08-04 16:30:13 -0300

    fix: rename directory of service tests

--- src/test/java/com/example/secure_password_vault/services/UserServiceTest.java
+++ src/test/java/com/example/secure_password_vault/services/UserServiceTest.java
@@ -1,4 +1,4 @@
-package com.example.secure_password_vault.user;
+package com.example.secure_password_vault.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
